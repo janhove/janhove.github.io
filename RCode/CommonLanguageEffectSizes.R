6c3aeac51b83bc5388ff32424d87b32856cdc010
janhove <janvanhove@gmail.com>
2016-11-17 09:39:21 +0100

    Better brute-force method for computing CLES

--- RCode/CommonLanguageEffectSizes.R
+++ RCode/CommonLanguageEffectSizes.R
@@ -5,7 +5,7 @@
 # for computing the probability that a random X is higher than a random Y.
 #
 # cles_brute.fnc() provides a brute-force method in which
-# a large number of random XY pairs is sampled (with replacement).
+# each X observation is paired to each Y observation.
 # The proportion of XY pairs where X is higher than Y is then computed and output.
 # Ties count as 0.5*TRUE.
 
@@ -13,12 +13,14 @@
 # janhove.github.io
 # jan.vanhove@unifr.ch
 
-# Last changed: 16 November 2016
+# Last changed: 17 November 2016.
+# Thanks to Guillaume Rousselet for suggesting a quicker and more exhaustive
+# brute-force solution.
 
 #########################################################
 ## BRUTE-FORCE METHOD
 
-# runs = how many random samples
+# runs = how many random samples (obsolete)
 # variable = variable for which to compute the CLES
 # group = grouping variable (two groups)
 # baseline = level in 'group': Probability that a member of this level of group selected at random has a score higher than a member not belonging to this level of group.
@@ -32,20 +34,30 @@ cles_brute.fnc <- function(runs = 1e5, variable, group, baseline, data) {
   # Select the observations for group 2
   y <- data[data[[group]] != baseline, variable]
   
+  
+  ## Old brute-force solution
   # - Randomly sample one member of group 1 and one member of group2
   #    and see if the member of group 1 has a higher score.
   # - Count ties as half-wins (0.5)
   # - Do this 'runs' number of times.
   
-  comparison <- replicate(runs,
-                          {s.x <- sample(x, 1)
-                           s.y <- sample(y, 1)
-                           ifelse(s.x == s.y, 
-                                  0.5,
-                                  s.x > s.y)})
+  # comparison <- replicate(runs,
+  #                         {s.x <- sample(x, 1)
+  #                          s.y <- sample(y, 1)
+  #                          ifelse(s.x == s.y, 
+  #                                 0.5,
+  #                                 s.x > s.y)})
   
-  # Compute the proportion of 'TRUE's and return it
-  return(mean(comparison))
+  # Matrix with difference between XY for all pairs (Guillaume Rousselet's suggestion)
+  m <- outer(x,y,FUN="-")
+
+  # Convert to booleans; count ties as half true.
+  m <- ifelse(m==0, 0.5, m>0)
+  
+  # Return proportion of TRUEs
+  qxly <- mean(m)
+  
+  return(qxly)
 }
 
 #########################################################
@@ -77,13 +89,12 @@ cles_comp.fnc <- function(variable, group, baseline, data) {
 #########################################################
 # Both
 
-cles.fnc <- function(runs = 1e5, variable, group, baseline, data, print = TRUE) {
-  cles_brute <- cles_brute.fnc(runs = runs, variable = variable, group = group, baseline = baseline, data = data)
+cles.fnc <- function(variable, group, baseline, data, print = TRUE) {
+  cles_brute <- cles_brute.fnc(runs = 0, variable = variable, group = group, baseline = baseline, data = data)
   cles_comp <- cles_comp.fnc(variable = variable, group = group, baseline = baseline, data = data)
   
   results <- list(algebraic = cles_comp,
-                  brute = cles_brute,
-                  runs = runs)
+                  brute = cles_brute)
   
   if(print == TRUE) {
   cat("Common-language effect size:",
@@ -94,7 +105,7 @@ cles.fnc <- function(runs = 1e5, variable, group, baseline, data, print = TRUE) 
       "\n    Algebraic method:   ", round(cles_comp, 2),
       "\n    Brute-force method: ", round(cles_brute, 2), 
       "\n",
-      "\n(brute-force method based on ", runs, " runs)\n",
+      # "\n(brute-force method based on ", runs, " runs)\n",
       sep = "")
   }
   
